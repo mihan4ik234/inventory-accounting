c76e6c90692192080d8e367880a18429d2ab98f0
Mihan4ik141 <Solopahan730@gmail.com>
2024-04-26 13:53:15 +0500

    addDataBase

--- API/obj/Debug/net8.0/IA-API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/IA-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IA-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9077d87d70ed7f25e653ee7c33b5c8c0dbff15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcd181aab1e0c792899e0914d869f5df14e00fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("IA-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IA-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
